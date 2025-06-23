2974d08cf631030eacd05c2dfa4b787007f3d07c
Santifer0803 <santifer0810@gmail.com>
2025-06-23 13:20:20 -0600

    feat: Reserva en millones

--- app.R
+++ app.R
@@ -5,8 +5,10 @@ library(plotly)
 library(readxl)
 library(shiny)
 
-tpx.h <- read_excel("data/VANUs 2025.xlsx", sheet = "Sobrevivencia H")
-tpx.m <- read_excel("data/VANUs 2025.xlsx", sheet = "Sobrevivencia M")
+tpx.h <-
+  read_excel("data/VANUs 2025.xlsx", sheet = "Sobrevivencia H")
+tpx.m <-
+  read_excel("data/VANUs 2025.xlsx", sheet = "Sobrevivencia M")
 
 tpx.h <- tpx.h[12:(nrow(tpx.h)), 3:ncol(tpx.h)]
 tpx.m <- tpx.m[12:(nrow(tpx.m)), 3:ncol(tpx.m)]
@@ -17,17 +19,20 @@ colnames(tpx.m) <- 1:ncol(tpx.m)
 r <- 0.036
 monto.min <- 31938.44
 
-calculo.vanu <- function(x, n, m, tpx){
-  
+calculo.vanu <- function(x, n, m, tpx) {
   # Se inicializa el vector que contendrá las VANUs
   vanu <- c()
   
   # Se itera sobre cada edad
-  for(edad in x){
-    
+  for (edad in x) {
     # Se calcula el VANU mensual para la edad correspondiente
-    vanu <- c(vanu, 12 * (sum(tpx[1:n, edad+1] / (1 + r)^(0:(n-1))) - 
-                            ((m-1) / (2*m) * (1-(tpx[n+1, edad+1]/((1+r)^n))))))
+    vanu <-
+      c(vanu, 12 * (sum(tpx[1:n, edad + 1] / (1 + r) ^ (0:(
+        n - 1
+      ))) -
+        ((m - 1) / (2 * m) * (1 - (
+          tpx[n + 1, edad + 1] / ((1 + r) ^ n)
+        )))))
     
   }
   
@@ -39,19 +44,15 @@ calculo.vanu <- function(x, n, m, tpx){
 vanu.h <- calculo.vanu(c(0:115), 115, 12, tpx.h)
 vanu.m <- calculo.vanu(c(0:115), 115, 12, tpx.m)
 
-retiro.prgrmd <- function(monto.acum, sexo, edad){
-  
+retiro.prgrmd <- function(monto.acum, sexo, edad) {
   # Se selecciona el vector de VANUs dependiendo del sexo
-  if(sexo == "Hombre" | sexo == "Masculino"){
-    
+  if (sexo == "Hombre" | sexo == "Masculino") {
     vanu <- vanu.h
     
-  } else if(sexo == "Mujer" | sexo == "Femenino"){
-    
+  } else if (sexo == "Mujer" | sexo == "Femenino") {
     vanu <- vanu.m
     
   } else{
-    
     stop("El sexo digitado no es válido")
     
   }
@@ -60,8 +61,8 @@ retiro.prgrmd <- function(monto.acum, sexo, edad){
   vector.edades <- c(edad)
   
   # Se calcula el monto de la pensión bajo esta modalidad
-  pension <- ifelse((monto.acum / vanu[edad+1]) < monto.min, 
-                    monto.min, monto.acum / vanu[edad+1])
+  pension <- ifelse((monto.acum / vanu[edad + 1]) < monto.min,
+                    monto.min, monto.acum / vanu[edad + 1])
   
   # Se inicializa el vector que almacenará la pensión mensual cada año
   vector.pensiones <- c(pension)
@@ -70,42 +71,44 @@ retiro.prgrmd <- function(monto.acum, sexo, edad){
   rends <- monto.acum * r - 12 * pension * r / 2
   
   # Se actualiza el monto acumulado (osea la reserva al final del año)
-  monto.acum <- ifelse((monto.acum + rends - 12*pension) < 0, 
-                       0, monto.acum + rends - 12*pension)
+  monto.acum <- ifelse((monto.acum + rends - 12 * pension) < 0,
+                       0,
+                       monto.acum + rends - 12 * pension)
   
   # Se inicializa el vector con la reserva al final de cada año
   vector.reservas <- c(monto.acum)
   
-  # Se repite el proceso iterando año a año hasta que se agote el fondo acumulado 
+  # Se repite el proceso iterando año a año hasta que se agote el fondo acumulado
   # o el afiliado llegue a los 115 años
-  while(monto.acum > 0 & edad <= 115){
-    
+  while (monto.acum > 0 & edad <= 115) {
     edad <- edad + 1
     
     vector.edades <- c(vector.edades, edad)
     
-    pension <- ifelse((monto.acum / vanu[edad+1]) < monto.min, 
-                      monto.min, monto.acum / vanu[edad+1])
+    pension <- ifelse((monto.acum / vanu[edad + 1]) < monto.min,
+                      monto.min,
+                      monto.acum / vanu[edad + 1])
     
     vector.pensiones <- c(vector.pensiones, pension)
     
     rends <- monto.acum * r - 12 * pension * r / 2
     
-    monto.acum <- ifelse((monto.acum + rends - 12*pension) < 0, 
-                         0, monto.acum + rends - 12*pension)
+    monto.acum <- ifelse((monto.acum + rends - 12 * pension) < 0,
+                         0,
+                         monto.acum + rends - 12 * pension)
     
     vector.reservas <- c(vector.reservas, monto.acum)
     
   }
   
   # Se retorna en un DataFrame la edad y la pensión año a año
-  data.frame(edad = vector.edades, pension = vector.pensiones, 
+  data.frame(edad = vector.edades,
+             pension = vector.pensiones,
              reserva.final = vector.reservas)
   
 }
 
-renta.prmnt <- function(monto.acum, edad, rends.3a){
-  
+renta.prmnt <- function(monto.acum, edad, rends.3a) {
   # Se inicializa el vector que almacenará las pensiones mensuales
   vector.pensiones <- c()
   
@@ -116,8 +119,7 @@ renta.prmnt <- function(monto.acum, edad, rends.3a){
   edad.aux <- edad
   
   # Se itera la edad en bloques de 3 años hasta los 115 años
-  while(edad.aux <= 115){
-    
+  while (edad.aux <= 115) {
     # Se calcula el monto de la pensión bajo esta modalidad
     pension <- max((sum(rends.3a) / 36), monto.min)
     
@@ -153,13 +155,13 @@ renta.prmnt <- function(monto.acum, edad, rends.3a){
   }
   
   # Se devuelve un DataFrame con las edades (de inicio a 115) y sus respectivas pensiones
-  data.frame(edad = edad:115, pension = vector.pensiones[1:(116-edad)],
-             reserva.final = vector.reservas[1:(116-edad)])
+  data.frame(edad = edad:115,
+             pension = vector.pensiones[1:(116 - edad)],
+             reserva.final = vector.reservas[1:(116 - edad)])
   
 }
 
 renta.tmprl <- function(capital.disp, sexo, edad) {
-  
   # Selección de tabla de supervivencia
   if (sexo == "Hombre" | sexo == "Masculino") {
     tpx <- tpx.h
@@ -177,7 +179,6 @@ renta.tmprl <- function(capital.disp, sexo, edad) {
   vector.rendimientos <- numeric(length(vector.edades))
   
   for (i in 1:(esperanza.restante + 1)) {
-    
     edad.actual <- vector.edades[i]
     
     if (capital.disp <= 0) {
@@ -191,7 +192,10 @@ renta.tmprl <- function(capital.disp, sexo, edad) {
     vanu <- calculo.vanu(edad.actual, n, 12, tpx)
     
     pension.por.renta.temp <- capital.disp / vanu
-    pension <- ifelse(pension.por.renta.temp < monto.min, monto.min, pension.por.renta.temp)
+    pension <-
+      ifelse(pension.por.renta.temp < monto.min,
+             monto.min,
+             pension.por.renta.temp)
     vector.pensiones[i] <- pension
     
     # cálculo de rendimiento
@@ -211,20 +215,25 @@ renta.tmprl <- function(capital.disp, sexo, edad) {
     vector.edades <- c(vector.edades, edad.extra)
     vector.pensiones <- c(vector.pensiones, capital.restante / 12)
     vector.capitales <- c(vector.capitales, 0)
-    vector.rendimientos <- c(vector.rendimientos, (capital.restante)*r-12*(capital.restante / 12)*r/2)
+    vector.rendimientos <-
+      c(vector.rendimientos,
+        (capital.restante) * r - 12 * (capital.restante / 12) * r / 2)
   }
   
   # Se quita el 0 final
   vector.edades <- vector.edades[-length(vector.edades)]
   vector.pensiones <- vector.pensiones[-length(vector.pensiones)]
-  vector.rendimientos <- vector.rendimientos[-length(vector.rendimientos)]
+  vector.rendimientos <-
+    vector.rendimientos[-length(vector.rendimientos)]
   vector.capitales <- vector.capitales[-length(vector.capitales)]
   
-  return(data.frame(
-    edad = vector.edades,
-    pension = vector.pensiones,
-    reserva.final = vector.capitales
-  ))
+  return(
+    data.frame(
+      edad = vector.edades,
+      pension = vector.pensiones,
+      reserva.final = vector.capitales
+    )
+  )
 }
 
 
@@ -306,8 +315,10 @@ server <- function(input, output, session) {
       } else if (input$modalidad == "Renta Permanente") {
         observe({
           if (!is.null(input$rend1) &&
-              !is.na(input$rend1) && is.numeric(input$rend1) && !is.null(input$rend2) &&
-              !is.na(input$rend2) && is.numeric(input$rend2) && !is.null(input$rend3) &&
+              !is.na(input$rend1) &&
+              is.numeric(input$rend1) && !is.null(input$rend2) &&
+              !is.na(input$rend2) &&
+              is.numeric(input$rend2) && !is.null(input$rend3) &&
               !is.na(input$rend3) && is.numeric(input$rend3)) {
             if (input$rend1 < 0) {
               updateNumericInput(session, "rend1", value = 0)
@@ -317,10 +328,12 @@ server <- function(input, output, session) {
               updateNumericInput(session, "rend3", value = 0)
             }
           } else if (is.null(input$rend1) ||
-                     is.na(input$rend1) || is.numeric(input$rend1)) {
+                     is.na(input$rend1) ||
+                     is.numeric(input$rend1)) {
             updateNumericInput(session, "rend1", value = 3000000)
           } else if (is.null(input$rend2) ||
-                     is.na(input$rend2) || is.numeric(input$rend2)) {
+                     is.na(input$rend2) ||
+                     is.numeric(input$rend2)) {
             updateNumericInput(session, "rend1", value = 3000000)
           } else {
             updateNumericInput(session, "rend3", value = 3000000)
@@ -342,20 +355,29 @@ server <- function(input, output, session) {
     })
   })
   
+  reserva <- reactive({
+    df <- resultado()
+    df[, 3] / 1000000
+  })
+  
   output$tabla_resultados <- renderDataTable({
     req(resultado)
     df <- round(resultado(), 2)
     
-    names(df) <- c("Edad", "Pensión", "Reserva final")
+    names(df) <-
+      c("Edad", "Pensión", "Reserva final")
     
-    datatable(df,
-              rownames = FALSE,
-              extensions = "Buttons",
-              options = list(
-                dom = 'Blfrtip',
-                buttons = c("excel"),
-                language = list(url = '//cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json')
-              ))
+    
+    datatable(
+      df,
+      rownames = FALSE,
+      extensions = "Buttons",
+      options = list(
+        dom = 'Blfrtip',
+        buttons = c("excel"),
+        language = list(url = '//cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json')
+      )
+    )
   })
   
   output$grafico_pension <- renderPlotly({
@@ -363,7 +385,8 @@ server <- function(input, output, session) {
     
     fig <- ggplot(df, aes(x = edad, y = pension)) +
       geom_line(color = "#18A777", linewidth = 1.2) +
-      labs(title = "Evolución de la Pensión Mensual", x = "Edad", y = "Pensión")
+      labs(title = "Evolución de la Pensión Mensual", x = "Edad (Años)", y = "Pensión (Colones)") +
+      theme_minimal()
     
     ggplotly(fig)
   })
@@ -371,9 +394,10 @@ server <- function(input, output, session) {
   output$grafico_reserva <- renderPlotly({
     df <- resultado()
     
-    fig <- ggplot(df, aes(x = edad, y = reserva.final)) +
+    fig <- ggplot(df, aes(x = edad, y = reserva())) +
       geom_line(color = "#18A777", linewidth = 1.2) +
-      labs(title = "Evolución de la Reserva", x = "Edad", y = "Reserva final")
+      labs(title = "Evolución de la Reserva", x = "Edad (Años)", y = "Reserva final (Millones de colones)") +
+      theme_minimal()
     
     ggplotly(fig)
   })
